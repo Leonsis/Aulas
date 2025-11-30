304c529f5324fc2c608736afb9ac2cf7ce94eec5
Leonsis <caiolenni@gmail.com>
2025-11-30 12:29:16 -0300

    Incuindo mais uma aula

--- PHP/Aula1.php
+++ PHP/Aula1.php
@@ -3,13 +3,19 @@
     <head>
         <meta charset="UTF-8">
         <title>Primeiro Exemplo PHP</title>
+        <style>
+            h2 {
+                color: #80a2ff;
+                text-shadow: 1px 1px 1px #000;
+            }
+        </style>
     </head>
     <body>
         <h1>Testando PHP</h1>
-        
+
         <!-- Logo abaixo temos a Super tag <?php?> que é usada para olocar o sódigo em php-->
         <?php
-            echo ("Olá mundo!"); // Com o comando echo, é possivel escrever caracteres ou variaveis.
+            echo ("<h2>Olá mundo!</h2>"); // Com o comando echo, é possivel escrever caracteres ou variaveis.
         ?>
     </body>
 </html>
